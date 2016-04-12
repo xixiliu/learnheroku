4c295c31e9da93633ffb60d4262cca9edd9de02d
xixiliu <xixiliu0913@gmail.com>
2016-04-11 20:40:19 -0400

    modify something

--- app.js
+++ app.js
@@ -36,6 +36,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 var routes = require('./routes/index');
 app.use('/', routes);
 
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
