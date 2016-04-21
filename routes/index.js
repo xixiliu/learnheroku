3b1b441c97edc81f0c76184d1726d8bb4c549789
xixiliu <xixiliu0913@gmail.com>
2016-04-20 22:15:13 -0400

    Add aws

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 var mongoose = require('mongoose');
 
 // our db model
-var Animal = require("../models/model.js");
+var Person = require("../models/person.js");
 var Eventlist = require("../models/event.js");
 /**
  * GET '/'
@@ -23,6 +23,22 @@ var Eventlist = require("../models/event.js");
 // });
 
 
+////////AWS XIXI
+// S3 File dependencies
+var AWS = require('aws-sdk');
+var awsBucketName = process.env.AWS_BUCKET_NAME;
+var s3Path = process.env.AWS_S3_PATH; // TODO - we shouldn't hard code the path, but get a temp URL dynamically using aws-sdk's getObject
+AWS.config.update({
+  accessKeyId: process.env.AWS_ACCESS_KEY,
+  secretAccessKey: process.env.AWS_SECRET_KEY
+});
+var s3 = new AWS.S3();
+
+// file processing dependencies
+var fs = require('fs');
+var multipart = require('connect-multiparty');
+var multipartMiddleware = multipart();
+
 
 /////////xixixi////////////////////
 
@@ -32,7 +48,11 @@ router.get('/', function(req,res){
   res.render('add.html')
 
 })
+router.get('/add-image', function(req,res){
 
+  res.render('addImage.html')
+
+})
 
 router.post('/api/create', function(req,res){
 
@@ -69,6 +89,116 @@ router.post('/api/create', function(req,res){
   })
 
 })
+
+
+
+
+////////XIXI/////////
+
+
+router.post('/api/create/image', multipartMiddleware, function(req,res){
+
+  console.log('the incoming data >> ' + JSON.stringify(req.body));
+  console.log('the incoming image file >> ' + JSON.stringify(req.files.image));
+
+  var personObj = {
+    name: req.body.name,
+    itpYear: req.body.itpYear,
+    interests: req.body.interests.split(','),
+    link: req.body.link,
+    slug : req.body.name.toLowerCase().replace(/[^\w ]+/g,'').replace(/ +/g,'-')
+  }
+
+  if (req.body.hasGlasses == 'yes') personObj['hasGlasses'] = true;
+  else personObj['hasGlasses'] = false;
+
+
+  // NOW, we need to deal with the image
+  // the contents of the image will come in req.files (not req.body)
+  var filename = req.files.image.name; // actual filename of file
+  var path = req.files.image.path; // will be put into a temp directory
+  var mimeType = req.files.image.type; // image/jpeg or actual mime type
+
+  // create a cleaned file name to store in S3
+  // see cleanFileName function below
+  var cleanedFileName = cleanFileName(filename);
+
+  // We first need to open and read the uploaded image into a buffer
+  fs.readFile(path, function(err, file_buffer){
+
+    // reference to the Amazon S3 Bucket
+    var s3bucket = new AWS.S3({params: {Bucket: awsBucketName}});
+
+    // Set the bucket object properties
+    // Key == filename
+    // Body == contents of file
+    // ACL == Should it be public? Private?
+    // ContentType == MimeType of file ie. image/jpeg.
+    var params = {
+      Key: cleanedFileName,
+      Body: file_buffer,
+      ACL: 'public-read',
+      ContentType: mimeType
+    };
+
+    // Put the above Object in the Bucket
+    s3bucket.putObject(params, function(err, data) {
+      if (err) {
+        console.log(err)
+        return;
+      } else {
+        console.log("Successfully uploaded data to s3 bucket");
+
+        // now that we have the image
+        // we can add the s3 url our person object from above
+        personObj['imageUrl'] = s3Path + cleanedFileName;
+
+        // now, we can create our person instance
+        var person = new Person(personObj);
+
+        person.save(function(err,data){
+          if(err){
+            var error = {
+              status: "ERROR",
+              message: err
+            }
+            return res.json(err)
+          }
+
+          var jsonData = {
+            status: "OK",
+            person: data
+          }
+
+          return res.json(jsonData);        
+        })
+
+      }
+
+    }); // end of putObject function
+
+  });// end of read file
+})
+
+function cleanFileName (filename) {
+
+    // cleans and generates new filename for example userID=abc123 and filename="My Pet Dog.jpg"
+    // will return "abc123_my_pet_dog.jpg"
+    var fileParts = filename.split(".");
+
+    //get the file extension
+    var fileExtension = fileParts[fileParts.length-1]; //get last part of file
+
+    //add time string to make filename a little more random
+    d = new Date();
+    timeStr = d.getTime();
+
+    //name without extension
+    newFileName = fileParts[0];
+
+    return newFilename = timeStr + "_" + fileParts[0].toLowerCase().replace(/[^\w ]+/g,'').replace(/ +/g,'_') + "." + fileExtension;
+
+}
 
 /////////xixixi////////////////////
 
@@ -180,28 +310,28 @@ router.get('/sample-page', function(req,res){
 //  * @return {Object} JSON
 //  */
 
-// router.get('/api/get', function(req, res){
+router.get('/api/get', function(req, res){
 
-//   // mongoose method to find all, see http://mongoosejs.com/docs/api.html#model_Model.find
-//   Animal.find(function(err, data){
-//     // if err or no animals found, respond with error 
-//     if(err || data == null){
-//       var error = {status:'ERROR', message: 'Could not find animals'};
-//       return res.json(error);
-//     }
+  // mongoose method to find all, see http://mongoosejs.com/docs/api.html#model_Model.find
+  Eventlist.find(function(err, data){
+    // if err or no animals found, respond with error 
+    if(err || data == null){
+      var error = {status:'ERROR', message: 'Could not find animals'};
+      return res.json(error);
+    }
 
-//     // otherwise, respond with the data 
+    // otherwise, respond with the data 
 
-//     var jsonData = {
-//       status: 'OK',
-//       animals: data
-//     } 
+    var jsonData = {
+      status: 'OK',
+      eventlist: data
+    } 
 
-//     res.json(jsonData);
+    res.json(jsonData);
 
-//   })
+  })
 
-// })
+})
 
 // /**
 //  * POST '/api/update/:id'
